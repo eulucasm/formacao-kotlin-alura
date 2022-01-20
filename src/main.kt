c1ff7f658d803046106742ea791d2f5ea07a7a12
eulucasm <lucaspoletis@gmail.com>
2022-01-19 22:05:25 -0300

    feat: aula sobre if else when

--- src/main.kt
+++ src/main.kt
@@ -5,9 +5,34 @@ fun main () {
     var saldo = 0.0
     saldo = 100 + 2.0
     saldo += 200
+    saldo = 0.0
+    saldo -= 1000
 
     println("Titular: $titular")
     println("Numero da conta $numeroConta")
     println("saldo da conta $saldo")
+
+    if (saldo > 0.0){
+        println("Conta é positiva. Saldo é $saldo")
+    } else if (saldo == 0.0){
+        println("Saldo é neutro")
+    } else {
+        println("Conta negativa. Saldo: $saldo")
+    }
+
+    //Mesma situação utilizando when
+
+    //mesma coisa do switch case
+    when {
+        saldo > 0.0 -> {
+            println("Conta é positiva. Saldo é $saldo")
+        }
+        saldo == 0.0 -> {
+            println("Saldo é neutro")
+        }
+        else -> {
+            println("Conta negativa. Saldo: $saldo")
+        }
+    }
 
 }
