fcc35b9a91186cb6b119c29cafeda9be56700b47
eulucasm <lucaspoletis@gmail.com>
2022-01-19 21:55:13 -0300

    feat: First commit

---
+++ src/main.kt
@@ -0,0 +1,13 @@
+fun main () {
+    println("Bem vindo ao BB")
+    val titular = "Lucas"
+    val numeroConta = 1000
+    var saldo = 0.0
+    saldo = 100 + 2.0
+    saldo += 200
+
+    println("Titular: $titular")
+    println("Numero da conta $numeroConta")
+    println("saldo da conta $saldo")
+
+}
