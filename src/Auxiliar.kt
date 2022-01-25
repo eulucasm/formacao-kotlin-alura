e656d24844d786d677d63990917339aef8856cab
eulucasm <lucaspoletis@gmail.com>
2022-01-25 17:17:15 -0300

    feat: mais sobre herança e polimorfismo

---
+++ src/Auxiliar.kt
@@ -0,0 +1,13 @@
+class Auxiliar(
+    nome: String,
+    cpf: String,
+    salario: Double
+) : Funcionario(nome, cpf, salario) {
+
+    override val bonificacao: Double
+        get() {
+            return salario * 0.1
+        }
+
+
+}
