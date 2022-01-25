689c88ae6071ec0de22a18b5cc71f49cdce4bac7
eulucasm <lucaspoletis@gmail.com>
2022-01-25 11:18:49 -0300

    feat: criando novas classes

--- src/main.kt
+++ src/main.kt
@@ -2,4 +2,14 @@ fun main() {
     println("Bem vindo ao Bytebank")
 
     testaComportamentosConta()
+
+    val alex = Funcionario(
+        "Alex",
+        "123456789-93",
+        1000.0
+    )
+    println("nome ${alex.nome}")
+    println("cpf ${alex.cpf}")
+    println("salario ${alex.salario}")
+    println("Bonificação ${alex.bonificacao()}")
 }
